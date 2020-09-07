4bea4a2ace94ef995af32b68dbe06c1b45409929
llalit <erlalit.lahoria@gmail.com>
2020-09-06 20:16:06 -0700

    Updates

---
+++ precache-manifest.1b42207724a4b4d62c070bca03a7340b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22c7fbd7d87dca8eda41b53aafac59f",
+    "url": "/ig-reels2/index.html"
+  },
+  {
+    "revision": "d418ff53291f17728d50",
+    "url": "/ig-reels2/static/css/main.79ae3d65.chunk.css"
+  },
+  {
+    "revision": "b868a58b88c6a10c15de",
+    "url": "/ig-reels2/static/js/2.cd74a05c.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/ig-reels2/static/js/2.cd74a05c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d418ff53291f17728d50",
+    "url": "/ig-reels2/static/js/main.10152cca.chunk.js"
+  },
+  {
+    "revision": "ae3aabdc0c31186b5764",
+    "url": "/ig-reels2/static/js/runtime-main.af373604.js"
+  }
+]);
